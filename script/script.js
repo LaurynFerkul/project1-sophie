8d8598c11a169486fa4631886a6e2f7f7536b659
Lauryn Ferkul <laurynferkul4@gmail.com>
2022-05-15 17:38:27 -0400

    made mobile nav interactive with JS

--- script/script.js
+++ script/script.js
@@ -1,3 +1,15 @@
+// query the DOM for the nav button and links
+const navButton = document.querySelector('.mobileNav');
+const nav = document.querySelector('.navMenu');
+
+console.log(nav);
+
 // add event listener (click) to our nav button
-// expand nav to show page option
-// 
+navButton.addEventListener('click', function (){
+    console.log('you clicked me');
+    // toggle nav between open and closed
+    navButton.classList.toggle('active');
+    nav.classList.toggle('active');
+
+})
+
