0658324dfedf784ba8efe0a39a23fdb6e3c63c1e
Lauryn Ferkul <laurynferkul4@gmail.com>
2022-05-16 18:08:44 -0400

    repaired broken link

--- script/script.js
+++ script/script.js
@@ -2,14 +2,12 @@
 const navButton = document.querySelector('.mobileNav');
 const nav = document.querySelector('.navMenu');
 
-console.log(nav);
-
 // add event listener (click) to our nav button
 navButton.addEventListener('click', function (){
     console.log('you clicked me');
     // toggle nav between open and closed
     navButton.classList.toggle('active');
     nav.classList.toggle('active');
-
 })
+
 
